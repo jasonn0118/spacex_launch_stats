93d68e40b49417c83946d87b3c2304ee6d461bd7
jasonn0118 <tlswoals2006@gmail.com>
2020-09-23 23:16:03 -0700

    simplify data fetching values to show

--- client/src/components/Launch.js
+++ client/src/components/Launch.js
@@ -23,50 +23,50 @@ const LAUNCH_QUERY = gql`
 const Launch = (props) => {
   let { flight_number } = props.match.params;
   flight_number = parseInt(flight_number);
-  const [launchData, setLaunchData] = useState(null);
   const { loading, error, data } = useQuery(LAUNCH_QUERY, {
-    variables: { flight_number }
+    variables: { flight_number },
   });
-
-  useEffect(() => {
-    if (data && data.launch) {
-        setLaunchData(data.launch);
-        console.log(launchData, '>>>>>LAUNCH DATA');
-    }
-  })
+  console.log(loading, '><>>>>>>LOADING');
+  console.log(data, '>>>>>>>>>>>>DATA');
 
   if (loading) return <p>Loading...</p>;
   if (error) {
     console.log(error);
     return <p>Something went wrong!</p>;
   }
+  const {
+    mission_name,
+    launch_year,
+    launch_success,
+    rocket: { rocket_id, rocket_name, rocket_type },
+  } = data.launch;
 
   return (
     <div>
       <h1 className='display-4 my-3'>
         <span className='text-dark'>Mission:</span>
-        {launchData && launchData.mission_name}
+        {mission_name}
       </h1>
       <h4 className="mb-3">Launch Details</h4>
       <ul className="list-group">
           <li className="list-group-item">
-                Flight Number: {launchData && launchData.flight_number}
+                Flight Number: {data.launch.flight_number}
           </li>
           <li className="list-group-item">
-                Launch Year: {launchData && launchData.launch_year}
+                Launch Year: {launch_year}
           </li>
           <li className="list-group-item">
                 Launch Successful: <span className={classNames({
-                    'text-success': launchData && launchData.launch_success,
-                    'text-danger': launchData && !launchData.launch_success
-                })}>{launchData && launchData.launch_success ? 'Yes' : 'No'}</span>
+                    'text-success': launch_success,
+                    'text-danger': !launch_success
+                })}>{launch_success ? 'Yes' : 'No'}</span>
           </li>
       </ul>
       <h4 className="my-3">Rocket Details</h4>
       <ul className="list-group">
-            <li className="list-group-item">Rocket ID: {launchData && launchData.rocket.rocket_id}</li>
-            <li className="list-group-item">Rocket Name: {launchData && launchData.rocket.rocket_name}</li>
-            <li className="list-group-item">Rocket Type: {launchData && launchData.rocket.rocket_type}</li>
+            <li className="list-group-item">Rocket ID: {rocket_id}</li>
+            <li className="list-group-item">Rocket Name: {rocket_name}</li>
+            <li className="list-group-item">Rocket Type: {rocket_type}</li>
       </ul>
       <hr />
       <Link to="/" className="btn btn-secondary">Back</Link>
